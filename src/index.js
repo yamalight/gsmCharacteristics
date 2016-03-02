0d9a490308d8b925517ff4b00bad56082d8a81aa
Tim Ermilov <yamalight@gmail.com>
2016-03-02 13:50:55 +0100

    improved splitting

---
+++ src/index.js
@@ -0,0 +1,67 @@
+/* eslint max-params: [2, 3] */
+/* eslint no-console: 0 */
+/* eslint complexity: [2, 11] */
+import {readFileSync, writeFileSync} from 'fs';
+import {processByType} from './process';
+
+const saveToFile = (obj, filename) => writeFileSync(filename, JSON.stringify(obj, null, 2), 'utf8');
+
+// get plain text
+const text = readFileSync('./gsmCharacteristics.csv').toString();
+// split by lines
+const lines = text
+    .replace(/\r\n/gm, '\n')
+    .split('\n')
+    .map(l => {
+        const kv = l.split(',');
+        const gsmid = kv.shift().toLowerCase();
+        const line = kv.join(',').replace(/\t/gm, ' ').replace(/\u0000/gm, '').trim().toLowerCase();
+        return {
+            gsmid,
+            line,
+        };
+    })
+    // remove empty lines
+    .filter(l => l.line.length > 0);
+// save shorts
+const SHORT_LENGTH = 10;
+const shortLines = lines.filter(l => l.line.length < SHORT_LENGTH);
+saveToFile(shortLines, './out/short.json');
+shortLines.length = 0;
+
+console.log('saved short!');
+
+// process
+const newLines = lines.filter(l => l.line.length > SHORT_LENGTH).map(processByType);
+
+// save untransformed
+const untransformed = newLines.filter(o => o.UNTRANSFORMED).map(v => {
+    const r = {
+        ...v,
+    };
+    delete r.UNTRANSFORMED;
+    return r;
+});
+saveToFile(untransformed, './out/not-processed.json');
+untransformed.length = 0;
+
+console.log('saved untransformed!');
+
+// save transformed
+const transformed = newLines.filter(o => !o.UNTRANSFORMED);
+const PAGE_SIZE = 200000;
+const pages = Math.floor(transformed.length / PAGE_SIZE);
+console.log('total size:', transformed.length);
+for (let i = 0; i <= pages; i++) {
+    const start = PAGE_SIZE * i;
+    let end = PAGE_SIZE * i + PAGE_SIZE;
+    if (end >= transformed.length) {
+        end = transformed.length - 1;
+    }
+    const tosave = transformed.slice(start, end);
+    saveToFile(tosave, `./out/processed_${i}.json`);
+    tosave.length = 0;
+    console.log('saved page:', i);
+}
+
+console.log('done!');
---
+++ src/process.js
@@ -0,0 +1,80 @@
+/* eslint max-params: [2, 3] */
+/* eslint no-console: 0 */
+/* eslint complexity: [2, 11] */
+
+// kv splitter
+const pairToKeyVal = (keyvalDelimitter, p) => {
+    // if no delimitter, return self as key
+    if (!keyvalDelimitter) {
+        return {NO_KEY: p};
+    }
+    const kv = p.split(keyvalDelimitter);
+    const key = (kv.shift() || '').trim().toLowerCase();
+    const joiner = typeof keyvalDelimitter === 'string' ? keyvalDelimitter : '';
+    const value = (kv.length === 1 ? kv[0] : kv.join(joiner) || '').trim().toLowerCase();
+    return {
+        [key]: value,
+    };
+};
+// splitter
+const splitter = (line, keyvalDelimitter, pairDelimitter) => {
+    if (pairDelimitter) {
+        const pairs = line.split(pairDelimitter);
+        return pairs
+            .map(p => pairToKeyVal(keyvalDelimitter, p))
+            .reduce((prev, cur) => Object.assign(prev, cur), {});
+    }
+
+    return pairToKeyVal(keyvalDelimitter, line);
+};
+
+const colonRegex = /:\s?([^\/])/;
+const semicolonRegex = /;\s?/;
+
+// process long lines by known types
+export const processByType = ({gsmid, line}) => {
+    let res = {};
+    // process
+    if (line.includes(': ') && line.includes('; ')) {
+        res = splitter(line, colonRegex, semicolonRegex);
+    } else if (line.includes(': ') && line.includes(';;')) {
+        res = splitter(line, colonRegex, ';;');
+    } else if (line.includes(': ') && line.includes(', ')) {
+        res = splitter(line, colonRegex, ', ');
+    } else if (line.includes(': ') && line.includes('. ')) {
+        res = splitter(line, colonRegex, '. ');
+    } else if (line.includes('=') && line.includes(', ')) {
+        res = splitter(line, '=', ', ');
+    } else if (line.includes('=') && line.includes('; ')) {
+        res = splitter(line, '=', '; ');
+    } else if (line.includes('=')) {
+        res = splitter(line, '=');
+    } else if (line.includes(' - ')) {
+        res = splitter(line, ' - ');
+    } else if (line.includes('- ')) {
+        res = splitter(line, '- ');
+    } else if (line.includes(':') || line.includes(': ')) {
+        res = splitter(line, colonRegex);
+    } else if (line.includes('_')) {
+        res = splitter(line, '_');
+    } else if (line.includes('; ') || line.includes(';')) {
+        res = splitter(line, undefined, semicolonRegex);
+    } else if (line.includes(', ')) {
+        res = splitter(line, undefined, ', ');
+    } else if (line.includes('. ')) {
+        res = splitter(line, undefined, '. ');
+    } else {
+        res = {UNTRANSFORMED: true};
+    }
+
+    // remove empty key-values
+    if (res.hasOwnProperty('') && !res['']) {
+        delete res[''];
+    }
+
+    return {
+        ...res,
+        gsmid,
+        originalLine: line,
+    };
+};
